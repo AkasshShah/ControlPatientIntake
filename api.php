337b99a93424b167ea94d94c5dbcd04afc5fb7a3
AkasshShah <akassh.shah77@gmail.com>
2020-10-14 21:38:48 -0400

    changed table structure and set up better output

--- api.php
+++ api.php
@@ -4,6 +4,11 @@
 
     // header("Access-Control-Allow-Origin: *");
 
+    $output = array(
+        "Status" => "OK",
+        "Data" => array()
+    );
+
     $json = file_get_contents('php://input');
     $input = json_decode($json, TRUE);
 
@@ -13,40 +18,78 @@
 
     if(!isTokenValid($token)){
         // Token is not valid
-        echo("Invalid Token");
+        $output["Status"] = "InvalidToken";
+        $output["Data"]["Token"] = $token;
+        echo(json_encode($output));
         exit();
     }
     // Token is valid
+
+    $permissionSymbols = array(
+        "write" => "W",
+        "read" => "R"
+    );
 
     $accessType = tokenAccessTypeRow($token);
 
     switch($type){
 
+        // case "something":
+        //     if(strpos($accessType["column"], $permissionSymbols["permission"]) !== false){
+        //         // Call function to input
+        //     }
+        //     else{
+        //         $output["Status"] = "PermissionDenied";
+        //         echo(json_encode($output));
+        //         exit();
+        //     }
+        // break;
+
         // Single Patient Information Entry
         case "SPIE":
-            if($accessType["PatientPermission"] == "W"){
+            if(strpos($accessType["PatientPermission"], $permissionSymbols["write"]) !== false){
                 // Call function to input
+            }
+            else{
+                $output["Status"] = "PermissionDenied";
+                echo(json_encode($output));
+                exit();
             }
         break;
 
         // Single Patient Insurance Information Entry
         case "SPIIE":
-            if($accessType["InsurancePermission"] == "W"){
+            if(strpos($accessType["InsurancePermission"], $permissionSymbols["write"]) !== false){
                 // Call function to input
+            }
+            else{
+                $output["Status"] = "PermissionDenied";
+                echo(json_encode($output));
+                exit();
             }
         break;
 
         // Single Patient Medical History Information Entry
         case "SPMHIE":
-            if($accessType["MedicalHistoryPermission"] == "W"){
+            if(strpos($accessType["MedicalHistoryPermission"], $permissionSymbols["write"]) !== false){
                 // Call function to input
+            }
+            else{
+                $output["Status"] = "PermissionDenied";
+                echo(json_encode($output));
+                exit();
             }
         break;
 
         // Single Patient Family History Information Entry
         case "SPFHIE":
-            if($accessType["FamilyHistoryPermission"] == "W"){
+            if(strpos($accessType["FamilyHistoryPermission"], $permissionSymbols["write"]) !== false){
                 // Call function to input
+            }
+            else{
+                $output["Status"] = "PermissionDenied";
+                echo(json_encode($output));
+                exit();
             }
         break;
     }
